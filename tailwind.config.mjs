c70be9de1d208aa6fec8cbd25d4f2dc4d9fd0810
Allenade <allenumunade@gmail.com>
2023-10-22 12:09:41 +0100

    fix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,7 @@ export default {
     extend: {
       screens: {
         sm: "390px", // Small screens, e.g., phones
-        md: "835px", // Medium screens, e.g., tablets
+        // md: "835px", // Medium screens, e.g., tablets
         lg: "1024px", // Large screens, e.g., laptops
         xl: "1280px", // Extra-large screens, e.g., desktops
         "2xl": "1536px", // Extra-extra-large screens, e.g., large desktops
